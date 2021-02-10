e189335ac5b3c0e324aa10de97bab65df6ce9610
arthurzakharov <zakharov.arthur@hotmail.com>
2021-02-11 01:16:45 +0300

    Server. Add readJson helper function

--- server/utils/index.js
+++ server/utils/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const readJson = (path, cb) => {
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
-      cb(err)
+      cb(err);
     } else {
       cb(null, data);
     }
