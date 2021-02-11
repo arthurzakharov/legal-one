483b2a4abfd86bcb7d6ecd5e4b33a5c2ea2103f3
arthurzakharov <zakharov.arthur@hotmail.com>
2021-02-11 18:12:13 +0300

    Client. Add interceptor for axios.

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -1,6 +1,23 @@
 import axios from 'axios';
 
+/**
+ * Another point for discussion. Personally I prefer to use instances 'cause they allow me to create
+ * several one and configure them with interceptors independently. Point to discuss on a code review
+ */
 export const API = axios.create({
   baseURL: '/api',
   timeout: 1000,
+  headers: {'Content-Type': 'application/json;charset=UTF-8'},
 });
+
+/**
+ * This can be a place for handling various scenarios such as catch a classify different
+ * error types. Or format data by normalizing it. Just a point to discuss on code review.
+ */
+API.interceptors.response.use(
+  (config) => ({
+    status: config.status,
+    data: config.data,
+  }),
+  (error) => Promise.reject(error)
+);
