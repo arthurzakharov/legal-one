4d76d2443617f9599474f6271ac3a62d7bf360d4
arthurzakharov <zakharov.arthur@hotmail.com>
2021-02-12 17:51:26 +0300

    Server. Add 404

--- server/index.js
+++ server/index.js
@@ -4,19 +4,27 @@ const bodyParser = require('body-parser');
 const baseRoutes = require('./routes/base');
 const agentRoutes = require('./routes/agent');
 const callRoutes = require('./routes/call');
+const notFoundController = require('./controllers/error');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/agent', agentRoutes);
 app.use('/api/call', callRoutes);
 app.use('/api', baseRoutes);
-app.get('*', (req, res) => {
-  return res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
+app.use(notFoundController.get404);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '..', 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
