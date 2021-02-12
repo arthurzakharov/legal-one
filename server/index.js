834ad49941ff57a5850f23a800ade90ff7103987
arthurzakharov <zakharov.arthur@hotmail.com>
2021-02-12 17:54:41 +0300

    Server. Add 404

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const baseRoutes = require('./routes/base');
 const agentRoutes = require('./routes/agent');
 const callRoutes = require('./routes/call');
-const notFoundController = require('./controllers/error');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,8 +14,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api/agent', agentRoutes);
 app.use('/api/call', callRoutes);
 app.use('/api', baseRoutes);
-app.use(notFoundController.get404);
-
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, '..', 'client/build')));
